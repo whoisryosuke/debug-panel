ea9ec475da308ba93f92daaede3fc3ed43687dd9
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2026-03-06 01:50:32 -0800

    :bug: add plugin for tsconfig paths

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "node:path";
 import { name } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [tsconfigPaths(), react(), dts()],
   build: {
     lib: {
       entry: resolve("src", "index.ts"),
